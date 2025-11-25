8f8040578cbada6d8c25617ef8d9821cf2010766
allan raymond <raymonda916@gmail.com>
2025-11-25 22:25:30 +0300

    Update auth actions

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -21,3 +21,28 @@ export async function login(formData: FormData) {
     revalidatePath('/', 'layout')
     redirect('/dashboard')
 }
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    return { error: error.message }
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/dashboard')
+}
+
+export async function signOut() {
+    const supabase = await createClient()
+    await supabase.auth.signOut()
+    revalidatePath('/', 'layout')
+    redirect('/login')
+}
